45b393fdaaab2d3092ba1d1f2baac994699343a7
Mirim <Mirim@PC-10>
2022-03-21 12:06:51 +0900

    Hello World %c로 찍기 1

--- cooding/cooding/00출력함수.c
+++ cooding/cooding/00출력함수.c
@@ -8,9 +8,22 @@ int main(void)
 	float sosu1 = 0.001592f;
 	//문자는 띄어쓰기포함해서 11개
 	//배열의 길이는 11+1(널문자) = 12
-	char mjyeol[12] = "Hello World";
-
+	char mjyeol[100] = "Hello World";
 	printf("정수 %d, 문자 %c, 더블 %lf, 플롯 %f\n"
 		, jungsu, munja, sosu, sosu1);
-	printf("%s\n", mjyeol);
+	printf("%c", mjyeol[0]);
+	printf("%c", mjyeol[1]);
+	printf("%c", mjyeol[2]);
+	printf("%c", mjyeol[3]);
+	printf("%c", mjyeol[4]);
+	printf("%c", mjyeol[5]);
+	printf("%c", mjyeol[6]);
+	printf("%c", mjyeol[7]);
+	printf("%c", mjyeol[8]);
+	printf("%c", mjyeol[9]);
+	printf("%c", mjyeol[10]);
+	printf("%c", mjyeol[11]);
+	//printf("%s\n", mjyeol);
+	// mjyeol를 %c로 찍어보자
+	//printf("%c\n", mjyeol);
 }
