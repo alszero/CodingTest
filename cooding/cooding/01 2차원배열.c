9747cbc8a78748dd6a19d90c4849707c9bfb3abb
Mirim <Mirim@PC-10>
2022-04-04 11:55:17 +0900

    좀더 유연한 반복문

--- cooding/cooding/01 2차원배열.c
+++ cooding/cooding/01 2차원배열.c
@@ -5,7 +5,9 @@ int main(void) {
 	//실수 1 idx 범위 초과 + 2부터 출력
 	
 	// 반복횟수를 12번 고정(유연하지 못한 코드)
-	for (int i = 0; i < 12; i++) 
+	// sizeof(arr) 배열 arr의 크기: 4*12 = 48
+	// sizeof(int) int형 변수의 크기: 4
+	for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
 	{
 		printf("%d ", arr[i]);
 	}
