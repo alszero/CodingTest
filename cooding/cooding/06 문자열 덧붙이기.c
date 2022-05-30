244f89a5639a31b5f1ff1683283c15eee2bcb0a2
Mirim <Mirim@PC-10>
2022-05-30 12:17:06 +0900

    문자열 덧붙이기 유연하게

--- cooding/cooding/06 문자열 덧붙이기.c
+++ cooding/cooding/06 문자열 덧붙이기.c
@@ -4,15 +4,20 @@
 void my_strcat(char*s1, char*s2) {
 	int s1_len = strlen(s1); //str1의 문자갯수
 	int s2_len = strlen(s2); //str2의 문자갯수
-	s1[5] = s2[0];
-	s1[6] = s2[1];
-	s1[7] = s2[2];
-	s1[8] = s2[3];
-	s1[9] = s2[4];
-	s1[10] = s2[5];
-	/*for (int i = s1_len; i < s1_len + s2_len; i++);
-		s1[i] = */
-	/*return 0;*/
+
+	//s1[5] = s2[0];
+	//s1[6] = s2[1];
+	//s1[7] = s2[2];
+	//s1[8] = s2[3];
+	//s1[9] = s2[4];
+	//s1[10] = s2[5];
+
+	// 5의 의미는 strlen(s1), 6번 반복하는 의미는 strlen(s2)
+	for (int i = s1_len; i < s1_len + s2_len; i++)
+	{
+		s1[s1_len + i] = s2[i];
+	} 
+	return 0;
 }
 int main(void)
 {
