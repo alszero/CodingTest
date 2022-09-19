4a1e618dc8b6d3767e3d6c418406c6a0a3fc3d26
Mirim <Mirim@PC-10>
2022-09-19 12:19:23 +0900

    07.동적할당 정적할당

--- cooding/cooding/1학기 수행평가 대비.c
+++ cooding/cooding/1학기 수행평가 대비.c
@@ -25,7 +25,29 @@ int main(void)
 	else
 		printf("양서영이 아닙니다. \n");
 
+	// 배열은 20개의 공간 만들기, 문자의 길이는 3
+	char str1[20] = "KMY";
 
+	// 문자의 길이를 구하는 함수 strlen
+	int length1 = strlen(str1);
+	printf("%d\n", length1);
+
+	// 문자열 복사 strcpy(복사대상, 복사문자열)
+	char copy_copy[10];
+	strcpy(copy_copy, str1);
+	printf("%s\n", copy_copy);
+
+	//문자열 덧붙이기 strcat(덧붙일 대상, 덧붙일 문자열)
+	strcat(copy_copy, "HIHI");
+	printf("%s\n", copy_copy);
+
+	//문자열 비교하기
+	if (strcmp(str, "KMKM") == 0) {
+		printf("김민영입니다.\n");
+	}
+	else {
+		printf("김민영이 아니네여\n");
+	}
 
 
 	// 문제 1 문자열의 길이 구하기
